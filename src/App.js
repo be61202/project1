6e7d9817247fe1b6f8a76bfd5c1bb5a8e0beacbc
zigzagwebpd <zigzagwebpd@gmail.com>
2024-03-24 03:25:49 +0900

    commit hyun2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import Join from "./join"
-
+import { BrowserRouter, Routes ,Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/join";
+import Find from "./pages/emailPwFind"
 function App() {
   return (
-    <div>
-      <Join/>
-    </div>
+   <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login/>}/>
+      <Route path="/join" element={<Signup/>}/>
+      <Route path="/idpw" element={<Find/>}/>
+    </Routes>
+   </BrowserRouter>
   );
 }
 
